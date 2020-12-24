3fe5c6ddffdd3544e105fab1fe7975537687374d
h4wldev <h4wldev@gmail.com>
2020-12-24 10:04:40 +0900

    pep8: fix pep8 errors

--- utils/exception.py
+++ utils/exception.py
@@ -7,23 +7,23 @@ def exception_wrapper(
         exc: 'HTTPException',
         excs: Tuple[Exception] = (),
         status_code: Union[int, HTTPStatusCode] = 400,
-        message_handler: Optional[Callable[[Exception], str]] = None,
-    ) -> Callable[..., Callable[..., Any]]:
-        def decorator(method: Callable[..., Any]) -> Callable[..., Any]:
-            def _(*args, **kwargs) -> Any:
-                try:
-                    return method(*args, **kwargs)
-                except excs as e:
-                    message: str = str(e)
+        message_handler: Optional[Callable[[Exception], str]] = None
+) -> Callable[..., Callable[..., Any]]:
+    def decorator(method: Callable[..., Any]) -> Callable[..., Any]:
+        def _(*args, **kwargs) -> Any:
+            try:
+                return method(*args, **kwargs)
+            except excs as e:
+                message: str = str(e)
 
-                    if message_handler:
-                        message = message_handler(e)
+                if message_handler:
+                    message = message_handler(e)
 
-                    raise exc(
-                        status_code=status_code,
-                        detail=message
-                    )
+                raise exc(
+                    status_code=status_code,
+                    detail=message
+                )
 
-            return _
+        return _
 
-        return decorator
+    return decorator
