fb852dc22ed8056a73c09c2ff6db717b39b07275
h4wldev <h4wldev@gmail.com>
2021-01-10 20:31:59 +0900

    setting: add testing setting

--- setting.py
+++ setting.py
@@ -9,13 +9,9 @@ setting: Dynaconf = Dynaconf(
     environments=True,
     settings_files=[
         'settings/secrets/.secrets.default.toml',
-        # 'settings/secrets/.secrets.development.toml',
-        # 'settings/secrets/.secrets.production.toml',
-        # 'settings/secrets/.secrets.testing.toml',
+        'settings/secrets/.secrets.testing.toml',
 
         'settings/setting.default.toml',
-        # 'settings/setting.development.toml',
-        # 'settings/setting.production.toml',
-        # 'settings/setting.testing.toml',
+        'settings/setting.testing.toml',
     ],
 )
