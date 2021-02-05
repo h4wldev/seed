d87befdbead21c642b86415036e07f74d5e4827b
h4wldev <h4wldev@gmail.com>
2021-02-05 18:41:43 +0900

    field: add user fields

---
+++ seed/fields/user_fields.py
@@ -0,0 +1,29 @@
+from typing import Optional
+
+from pydantic import BaseModel, Field, validator
+
+from seed.utils.regex_patterns import email_pattern
+
+
+class RegisterFields(BaseModel):
+    code: str = Field(..., min_length=20)
+    email: str = Field(..., max_length=100)
+    username: str = Field(..., min_length=3, max_length=50)
+    display_name: str = Field(..., min_length=2, max_length=50)
+    email_promotion: Optional[bool] = Field(True)
+    email_notification: Optional[bool] = Field(True)
+
+    @validator('email')
+    def email_pattern(
+        cls,
+        value: str
+    ) -> str:
+        if not email_pattern.match(value):
+            raise ValueError('must email pattern')
+
+        return value
+
+
+class SocialInfoFields(BaseModel):
+    social_id: str = Field(...)
+    provider: str = Field(...)
