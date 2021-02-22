c2a3a8deda7e7c7df1ca7fe01aa6aea538f5c8fc
h4wldev <h4wldev@gmail.com>
2021-02-22 14:07:31 +0000

    setting: change setting includes logic

--- seed/setting.py
+++ seed/setting.py
@@ -9,11 +9,7 @@ setting: Dynaconf = Dynaconf(
     environments=True,
     settings_files=[
         'seed/settings/.secrets.default.toml',
-        'settings/secrets/.secrets.development.toml',
-        'settings/secrets/.secrets.testing.toml',
-
-        'seed/settings/setting.default.toml',
-        'settings/setting.development.toml',
-        'settings/setting.testing.toml',
+        'seed/settings/setting.default.toml'
     ],
+    DYNACONF_INCLUDE=['../../settings/*.toml']
 )
